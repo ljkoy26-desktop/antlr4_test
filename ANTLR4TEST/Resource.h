2503a02f34725833ac9ac8af7ffe6f5ea8dffdba
ljkoy26-notebook <61034536+ljkoy26-notebook@users.noreply.github.com>
2026-01-29 14:56:17 +0900

    신택스 타입 체크 버튼 추가

--- ANTLR4TEST/Resource.h
+++ ANTLR4TEST/Resource.h
@@ -9,6 +9,7 @@
 #define IDC_EDIT_SQL                    1000
 #define IDC_BUTTON_PARSE                1001
 #define IDC_EDIT_TRACE                  1002
+#define IDC_BUTTON_TYPE                 1003
 
 // Next default values for new objects
 // 
